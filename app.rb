8555f94aaef3f0858a4c32525dbbe277d29b8d15
rafil <rafilsafargaliev@gmail.com>
2024-02-04 11:02:33 +0300

    select comments to @comments variable

--- app.rb
+++ app.rb
@@ -72,11 +72,17 @@ end
 get '/details/:post_id' do
 	# получаем переменную из url'a
 	post_id = params[:post_id]
+	
 	# получаем список постов
 	# (у нас будет только один пост)
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
+	
 	# выбираем этот один пост в переменную @row
 	@row = results[0]
+
+	# выбираем комментарии для нашего поста
+	comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+
 	# возвращаем представлеие details.erb
 	erb :details
   end
